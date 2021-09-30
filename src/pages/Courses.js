7a559de4a0d41bc55e948d0bc15fe9570f510a54
fverne <fverne@baseret.dk>
2021-09-30 12:03:48 +0200

    added course pages, set z index for the nav-menu

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,11 +1,50 @@
-import React from "react";
+import {useObserver} from "mobx-react-lite";
+import {Button, Grid, Paper} from "@mui/material";
+import Box from "@mui/material/Box";
+import {useCallback, useState} from "react";
+import {useHistory} from 'react-router-dom';
+import CourseStore from "../store/CourseStore";
 
-function Courses(){
-    return(
-        <div className='courses'>
-            <h1>Courses</h1>
-        </div>
+
+
+const courseStore = new CourseStore();
+
+export default function Courses() {
+    return useObserver(() =>
+        <Box padding={2}>
+            <h1> Course Page</h1>
+            <Grid container spacing={2} padding={2}>
+                {courseStore.courses.map((course)=>
+                    <Grid item
+                          xs={12}
+                          sm={4}
+                    >
+                        <CoursePaper course={course}/>
+                    </Grid>
+                )}
+            </Grid>
+
+            <Button sx={{px: 2}} onClick={() => courseStore.courses.push({name: "Test Course", maxcap: 60, id: Math.ceil(Math.max(Math.random()*99999, 10000)), weekday: "Random Weekday", time: "08:00 - 22:00"})}> Add Test Course</Button>
+        </Box>
     )
 }
 
-export default Courses
+function CoursePaper({course}) {
+    const [raiseLevel, setRaiseLevel] = useState(1);
+
+    const history = useHistory();
+    const handleOnClick = useCallback(() => history.push(`/courses/${course.id}`), [history, course.id]);
+
+    return (
+        <Paper
+            sx={{py: 2, px: 2, cursor: "pointer"}}
+            elevation={raiseLevel}
+            onMouseEnter={() => setRaiseLevel(4)}
+            onMouseLeave={() => setRaiseLevel(1)}
+            onClick={() => {handleOnClick()}}
+        >
+            <h2>{course.name} ({course.id || "ERROR: NO COURSE ID"})</h2>
+            <p>{course.weekday + ", " + course.time}</p>
+        </Paper>
+    )
+}
